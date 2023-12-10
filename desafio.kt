e7b9cb4ca84b0eaab1b717c2fc72d668f4cc4c63
moura1001 <gmouraneto01@gmail.com>
2023-12-10 17:31:41 -0300

    impl: cenário de uso

--- desafio.kt
+++ desafio.kt
@@ -4,7 +4,7 @@ enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 data class Usuario(val nome: String)
 
-data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracao: Int = 1)
 
 data class Modulo(val nome: String, val cursos: List<ConteudoEducacional>)
 
@@ -17,6 +17,27 @@ data class Formacao(val nome: String, val nivel: Nivel, val duracao: Int, val co
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val modulo = Modulo(
+        nome = "Aprendendo Kotlin na Prática em Sua Documentação Oficial",
+        cursos = listOf(
+            ConteudoEducacional("Conhecendo o Kotlin e Sua Documentação Oficial", Nivel.BASICO),
+            ConteudoEducacional("Introdução Prática à Linguagem de Programação Kotlin", Nivel.BASICO, 2),
+            ConteudoEducacional("Estruturas de Controle de Fluxo e Coleções em Kotlin", Nivel.BASICO, 2),
+            ConteudoEducacional("POO e Tipos de Classes na Prática com Kotlin", Nivel.BASICO, 2),
+            ConteudoEducacional("O Poder das Funções em Kotlin", Nivel.BASICO, 2),
+            ConteudoEducacional("Tratamento de Exceções em Kotlin", Nivel.INTERMEDIARIO, 2),
+            ConteudoEducacional("Abstraindo Formações da DIO Usando POO com Kotlin", Nivel.BASICO, 2)
+        )
+    )
+
+    val formacao = Formacao(
+        nome = "Desenvolvimento Backend com Kotlin", nivel = Nivel.BASICO, duracao = 52,
+        conteudos = listOf(modulo)
+    )
+
+    formacao.matricular(Usuario("AlunoA"))
+    formacao.matricular(Usuario("AlunoB"))
+    formacao.matricular(Usuario("AlunoC"))
+
+    println(formacao)
 }
